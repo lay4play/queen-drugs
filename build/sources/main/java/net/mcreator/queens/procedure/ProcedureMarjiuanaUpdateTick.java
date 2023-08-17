9f6501a96db47cae2ab1aa7460ac20108314e298
queenriccardo <64636433+queenriccardo@users.noreply.github.com>
2023-08-17 02:00:58 +0200

    .
    semi

---
+++ build/sources/main/java/net/mcreator/queens/procedure/ProcedureMarjiuanaUpdateTick.java
@@ -0,0 +1,13 @@
+package net.mcreator.queens.procedure;
+
+import net.mcreator.queens.ElementsQueens;
+
+@ElementsQueens.ModElement.Tag
+public class ProcedureMarjiuanaUpdateTick extends ElementsQueens.ModElement {
+	public ProcedureMarjiuanaUpdateTick(ElementsQueens instance) {
+		super(instance, 32);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+	}
+}
---
+++ src/main/java/net/mcreator/queens/procedure/ProcedureMarjiuanaPlantRightClicked.java
@@ -0,0 +1,46 @@
+package net.mcreator.queens.procedure;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+
+import net.mcreator.queens.ElementsQueens;
+
+@ElementsQueens.ModElement.Tag
+public class ProcedureMarjiuanaPlantRightClicked extends ElementsQueens.ModElement {
+	public ProcedureMarjiuanaPlantRightClicked(ElementsQueens instance) {
+		super(instance, 32);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+	}
+
+	@SubscribeEvent
+	public void onBonemeal(BonemealEvent event) {
+		EntityPlayer entity = event.getEntityPlayer();
+		int i = event.getPos().getX();
+		int j = event.getPos().getY();
+		int k = event.getPos().getZ();
+		World world = event.getWorld();
+		ItemStack itemstack = event.getStack();
+		java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("itemstack", itemstack);
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
